7b7dd065378269d46af9272327816f1fdb2643e2
Ethan Levy <ethanlevy@Anubis.public.alan.lan>
2022-09-05 20:46:29 -0400

    UpdateBachman

--- app.py
+++ app.py
@@ -25,12 +25,30 @@ def songread():
 def backpacking_photos():
     return render_template('backpacking_photos.html')
 
+@app.route("/contact_form")
+def contact_form():
+    return render_template('contact-form.html')
+
+
+@app.route("/Resume")
+def Resume():
+    return render_template('Resume.html')
+
+
+
 @app.route("/")
 def home():
     url_for('static', filename='playlist_store.js')
     url_for('static', filename='playlist.js')
+    url_for('static', filename='contact-form-process.php')
     url_for('static', filename='style.css')
     url_for('static', filename='levypedia.css')
     url_for('static', filename='Profile-Picture.jpeg')
     url_for('static', filename='backpacking_photos.html')
+    url_for('static', filename='ResumeWeb.pdf')
+    url_for('static', filename='Resume.html')
+    url_for('static', filename='CLS.pdf')
+    url_for('static', filename='contact-form.css')
+    url_for('static', filename='contact-form.html')
+    
     return render_template('index.html')
